bb251ae12bb444c7ceada577953e247fd3f42269
Pavel Terekhov <pavel.a.terekhov@gmail.com>
2024-03-23 01:55:15 +0700

    Label (first:) + continue in a nested loop

--- script.js
+++ script.js
@@ -55,9 +55,24 @@ const length = 7;
 
 for (let i = 1; i < length; i++) {
     for (let j = 1; j < i; j++) {
-        result = '*';
+        result += '*';
     }
-    result += '/n';
+    result += '\n';
 }
 
 console.log(result);
+
+
+// Метка (first:) + continue во вложенном цикле 
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) continue
+            console.log(`Third level ${k}`);
+        }
+    }
+
+}
