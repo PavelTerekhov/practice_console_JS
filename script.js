c600b43ba71ef2fefac4e47f7dd0df64be706e08
Pavel Terekhov <pavel.a.terekhov@gmail.com>
2024-03-24 23:21:00 +0700

    Advanced tasks for using cycles and conditions

--- script.js
+++ script.js
@@ -141,3 +141,49 @@ for (let i = 5; i < 11; i++) {
 }
 
 console.log(arrayOfNumbers);
+
+
+// 1) Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд.
+// Должен получиться точно такой же массив
+
+
+//1
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const resultArr = [];
+
+// Пишем решение вот тут
+for (let i = 0; i < arr.length; i++) {
+  resultArr[i] = arr[i];
+}
+
+console.log(resultArr);
+
+// 2) Измените данный массив так, чтобы все числа были увеличены в 2 раза, а если попадается строка строка - то к ней было добавлено " - done".
+// Для определения типа данных используйте typeof();
+// Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
+
+//2
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+for (let i = 0; i < data.length; i++) {
+    if (typeof(data[i]) === 'number') {
+        data[i] = data[i] * 2;
+    } 
+    if (typeof(data[i]) === 'string') {
+        data[i] = data[i] + " - done"
+    }
+}
+console.log(data);
+
+// 3) Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
+// Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
+
+//3 
+const dataArr = [5, 10, 'Shopping', 20, 'Homework'];
+const resultDataArr = [];
+
+for (let i = 1; i <= dataArr.length; i++) {
+    resultDataArr[i - 1] = dataArr[dataArr.length - i]
+}
+
+console.log(resultDataArr);
